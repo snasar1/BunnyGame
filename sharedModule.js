5d830444ee3218a49466f7eb2c58578c7fc43182
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-10-31 16:09:24 -0400

    Delete sharedModule.js

--- sharedModule.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// Load levels data from 'level.json'
-const levels = JSON.parse(fs.readFileSync('level.json'));
-
-// Initialize highScores
-let highScores = [];
-
-module.exports = { levels, highScores };
